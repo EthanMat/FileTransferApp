c5b7f4cc2a9b19074f6aec2b5221e690f9f4b92e
Ethan Mathew <ethmatt09@gmail.com>
2024-04-13 11:56:58 -0500

    intital commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from _tkinter import ttk
+import asyncio
+import websockets
+
+#create a window and set its geometry
+window = tk.Tk()
+window.geometry("800x500")
+
+#run main loop
+window.mainloop()
