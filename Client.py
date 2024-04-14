a65aefc81a1365244097e417a7dfdfcd3e743a63
Ethan Mathew <ethmatt09@gmail.com>
2024-04-14 12:05:38 -0500

    window customizations

--- Client.py
+++ Client.py
@@ -5,12 +5,10 @@ import File
 #create a window and set its geometry
 window = tk.Tk()
 window.geometry("800x500")
-test = File.File.file_to_binary_string("File.py")
-try:
-    print(test)
-    File.File.binary_string_to_file(test, "C:\\Users\\ethma\\Documents\\Ethan_work\\home\\Ethan_codes\\python\\FileTransferApp\\test.py")
-except MemoryError as e:
-    print("This file is too large to Process!")
+
+#window customizions
+window.iconbitmap("Icon.ico")
+window.title("Wireless File Transfer")
 
 #run main loop
 window.mainloop()
