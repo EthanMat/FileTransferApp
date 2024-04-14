29693973c8e8943188846bd078ce98d8716b60c9
Ethan Mathew <ethmatt09@gmail.com>
2024-04-14 15:17:44 -0500

    Started welcome page

--- Client.py
+++ Client.py
@@ -1,14 +1,26 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, font
+from PIL import Image, ImageTk
 import File
 
 #create a window and set its geometry
 window = tk.Tk()
 window.geometry("800x500")
 
+window.minsize(300,100)
+
 #window customizions
 window.iconbitmap("Icon.ico")
 window.title("Wireless File Transfer")
+
+image = Image.open('logo.png').resize((128, 128))
+image = ImageTk.PhotoImage(image)
+
+logo = tk.Label(window, image=image) # type: ignore
+logo.pack()
+
+welcome_label = ttk.Label(window, text="Welcome", font=font.BOLD)
+welcome_label.pack()
 
 #run main loop
 window.mainloop()
