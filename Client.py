30da7fcf58507d1092a0ad8a653458926ea95634
Ethan Mathew <ethmatt09@gmail.com>
2024-04-27 00:28:14 -0500

    Learned how to use CTkFont

--- Client.py
+++ Client.py
@@ -3,7 +3,7 @@ import customtkinter as ctk
 import tkinter.messagebox as tkmb 
 from tkinter import ttk, font
 from PIL import Image
-import File
+from File import File
 import Network
 
 # Selecting GUI theme - dark, light , system (for system default) 
@@ -14,9 +14,9 @@ ctk.set_default_color_theme("green")
 
 #create a window and set its geometry
 window = ctk.CTk()
-window.geometry("600x520")
+window.geometry("500x530")
 
-window.minsize(300,500)
+window.minsize(60,50)
  
 #window customizions
 window.iconbitmap("Icon.ico")
@@ -31,13 +31,13 @@ image = ctk.CTkImage(light_image=Image.open("logo.png"),
                     size=(128, 128))
 
 logo = ctk.CTkLabel(window, image = image, text = "")
-logo.pack(pady=75)
+logo.pack(pady=70)
 
-welcome_label = ctk.CTkLabel(window, text="Welcome")
-welcome_label.pack()
+welcome_label = ctk.CTkLabel(window, text="Welcome", font = ("Trebuchet MS", 42, "bold"))
+welcome_label.pack(padx = 10)
 
 login_frame = ctk.CTkFrame(window)
-login_frame.pack(pady=20,padx=200,fill='both',expand=True)
+login_frame.pack(pady=20,padx=150,fill='both',expand=True)
 
 username = ctk.CTkEntry(login_frame, placeholder_text = "Username")
 username.pack(pady = 20)
